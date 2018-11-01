909ac7f1b676ed3eb1faa7e4afb0f1322ac325d0
yangri <827969491@qq.com>
2018-11-01 21:13:11 +0800

    修改bug

--- javatv/jtm-query-service/src/main/java/org/jtm/query/service/QueryApplication.java
+++ javatv/jtm-query-service/src/main/java/org/jtm/query/service/QueryApplication.java
@@ -1,14 +1,21 @@
 package org.jtm.query.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Hello world!
  *
  */
+@RestController
 @EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class QueryApplication 
 {
@@ -16,4 +23,13 @@ public class QueryApplication
     {
     	SpringApplication.run(QueryApplication.class, args);
     }
+    
+    @Value("${jtm.server.port}")
+	String foo;
+    
+	@RequestMapping("/query1")
+	public String hi(){
+		
+		return foo;
+	}
 }
