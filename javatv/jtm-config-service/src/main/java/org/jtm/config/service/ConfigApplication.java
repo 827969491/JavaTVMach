cc28a5a32ddd4b905c7bcc0e0c68de4d6e8792f7
yangri <827969491@qq.com>
2018-11-01 19:46:46 +0800

    修改bug

--- javatv/jtm-config-service/src/main/java/org/jtm/config/service/ConfigApplication.java
+++ javatv/jtm-config-service/src/main/java/org/jtm/config/service/ConfigApplication.java
@@ -2,15 +2,15 @@ package org.jtm.config.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Hello world!
  *
  */
 @EnableConfigServer
-@EnableDiscoveryClient
+@EnableEurekaClient
 @SpringBootApplication
 public class ConfigApplication 
 {
--- javatv/jtm-query-service/src/main/java/org/jtm/query/service/QueryApplication.java
+++ javatv/jtm-query-service/src/main/java/org/jtm/query/service/QueryApplication.java
@@ -2,11 +2,13 @@ package org.jtm.query.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Hello world!
  *
  */
+@EnableEurekaClient
 @SpringBootApplication
 public class QueryApplication 
 {
